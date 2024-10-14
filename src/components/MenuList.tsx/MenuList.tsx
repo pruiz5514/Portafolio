a7bd566ab6ffbfe70bc1fd446f47390460727d95
Pablo Ruiz <pablo_ruiza@hotmail.com>
2024-10-13 19:31:53 -0500

    internacionalization is done

--- src/components/MenuList.tsx/MenuList.tsx
+++ src/components/MenuList.tsx/MenuList.tsx
@@ -1,11 +1,25 @@
-import { setLanguage } from '../../../utils/language';
+import { getLanguage, setLanguage } from '../../../utils/language';
 import {useTranslations} from 'next-intl';
 import './MenuList.scss'
+import { useEffect, useState } from 'react';
 
 const MenuList = () => {
+  const [language, setLanguageState] = useState('');
+
+  useEffect(()=>{
+    const fetchLanguage = async () =>{
+        let lang = await getLanguage() ; 
+        setLanguageState(String(lang))
+    }
+    fetchLanguage()
+  },[]);
+
+  console.log(language);
+  
   const handleSelect = (event: React.ChangeEvent<HTMLSelectElement>)=>{
     const lang = event.target.value;
     setLanguage(lang);
+    setLanguageState(lang)
   } 
   const t = useTranslations('Header');
 
@@ -16,7 +30,7 @@ const MenuList = () => {
         <li>{t('projects')}</li>
         <li>{t('contact')}</li>
         <li>
-            <select className='language-select' onChange={handleSelect}>
+            <select className='language-select' onChange={handleSelect} value={language}>
                 <option value="en"> English</option>
                 <option value="es"> Español </option>
             </select>
--- utils/language.ts
+++ utils/language.ts
@@ -4,3 +4,8 @@ import { cookies } from "next/headers"
 export const setLanguage = (lang:string)=> {
     cookies().set('language', lang);
 }
+
+export const  getLanguage = async()=>{
+    const language = cookies().get('language')?.value;
+    return language;
+}
