5c58e32282f6f59a48d95b0f3060ee584a7450e1
Pablo Ruiz <pablo_ruiza@hotmail.com>
2024-10-18 10:56:24 -0500

    change colors

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -6,17 +6,20 @@ const AboutMe = () => {
   const t = useTranslations('About');
   return (
     <section className='about-me_section'>
+      <div className='about-me_container'>
         <h1 className='about-me_title'>About me</h1>
-        <div className='about-info_container'>
-            <div className='about-text_container'>
-                <p className='description_p'> {t("description1")}</p>
-                <p className='description_p'> {t("description2")}</p>
-            </div>
-            <div className='about-model_container'>
-                <Model/>
-            </div>
-            
-        </div>
+          <div className='about-info_container'>
+              <div className='about-text_container'>
+                  <p className='description_p'> {t("description1")}</p>
+                  <p className='description_p'> {t("description2")}</p>
+              </div>
+              <div className='about-model_container'>
+                  <Model/>
+              </div>
+              
+          </div>
+      </div>
+        
     </section>
   )
 }
